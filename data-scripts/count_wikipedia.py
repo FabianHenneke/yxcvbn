36e4f67a40862e503f428357825fb3a5856fab70
Fabian Henneke <fabian@henneke.me>
2018-04-09 11:23:25 +0200

    Port count_wikipedia.py to Python 3

--- data-scripts/count_wikipedia.py
+++ data-scripts/count_wikipedia.py
@@ -12,7 +12,7 @@ import warnings
 from unidecode import unidecode
 
 def usage():
-    print '''
+    print('''
 tokenize a directory of text and count unigrams.
 
 usage:
@@ -48,7 +48,7 @@ and articles but not previous revisions, edit history, and metadata.
 Then run:
 ./WikiExtractor.py -o en_sents --no-templates enwiki-20151002-pages-articles.xml.bz2
 
-''' % sys.argv[0]
+''' % sys.argv[0])
 
 SENTENCES_PER_BATCH = 500000 # after each batch, delete all counts with count == 1 (hapax legomena)
 PRE_SORT_CUTOFF = 300        # before sorting, discard all words with less than this count
@@ -110,7 +110,7 @@ class TopTokenCounter(object):
 
     def pre_sort_prune(self):
         under_cutoff = set()
-        for token, count in self.count.iteritems():
+        for token, count in self.count.items():
             if count < PRE_SORT_CUTOFF:
                 under_cutoff.add(token)
         for token in under_cutoff:
@@ -118,7 +118,7 @@ class TopTokenCounter(object):
         self.legomena = set()
 
     def get_sorted_pairs(self):
-        return sorted(self.count.items(), key=operator.itemgetter(1), reverse=True)
+        return sorted(list(self.count.items()), key=operator.itemgetter(1), reverse=True)
 
     def get_ts(self):
         return datetime.datetime.now().strftime("%b %d %Y %H:%M:%S")
@@ -129,7 +129,7 @@ class TopTokenCounter(object):
 
 def main(input_dir_str, output_filename):
     counter = TopTokenCounter()
-    print counter.get_ts(), 'starting...'
+    print(counter.get_ts(), 'starting...')
     lines = 0
     for root, dirs, files in os.walk(input_dir_str, topdown=True):
         if not files:
@@ -137,28 +137,22 @@ def main(input_dir_str, output_filename):
         for fname in files:
             path = os.path.join(root, fname)
             for line in codecs.open(path, 'r', 'utf8'):
-                with warnings.catch_warnings():
-                    # unidecode() occasionally (rarely but enough to clog terminal outout)
-                    # complains about surrogate characters in some wikipedia sentences.
-                    # ignore those warnings.
-                    warnings.simplefilter('ignore')
-                    line = unidecode(line)
                 tokens = nltk.word_tokenize(line)
                 counter.add_tokens(tokens)
                 lines += 1
                 if lines % SENTENCES_PER_BATCH == 0:
                     counter.batch_prune()
-                    print counter.get_stats()
-                    print 'processing: %s' % path
-    print counter.get_stats()
-    print 'deleting tokens under cutoff of', PRE_SORT_CUTOFF
+                    print(counter.get_stats())
+                    print('processing: %s' % path)
+    print(counter.get_stats())
+    print('deleting tokens under cutoff of', PRE_SORT_CUTOFF)
     counter.pre_sort_prune()
-    print 'done'
-    print counter.get_stats()
-    print counter.get_ts(), 'sorting...'
+    print('done')
+    print(counter.get_stats())
+    print(counter.get_ts(), 'sorting...')
     sorted_pairs = counter.get_sorted_pairs()
-    print counter.get_ts(), 'done'
-    print 'writing...'
+    print(counter.get_ts(), 'done')
+    print('writing...')
     with codecs.open(output_filename, 'w', 'utf8') as f:
         for token, count in sorted_pairs:
             f.write('%-18s %d\n' % (token, count))
